e700decc1f32bc57c5a8d5d15287865b9cad24ac
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-07-10 10:09:32 +0530

    Update CursorEvents.js

--- js/CursorEvents.js
+++ js/CursorEvents.js
@@ -22,24 +22,10 @@ AFRAME.registerComponent("cursor-listener", {
   },
   handleMouseEnterEvents: function () {
     //Cursor 'mouseenter' Events
-    this.el.addEventListener("mouseenter", () => {
-      this.handlePlacesListState();
-    });
+   
   },
   handleMouseLeaveEvents: function () {
     //Cursor 'mouseleave' Events
-    this.el.addEventListener("mouseleave", () => {
-      const { selectedItemId } = this.data;
-      if (selectedItemId) {
-        const el = document.querySelector(`#${selectedItemId}`);
-        const id = el.getAttribute("id");
-        if (id == selectedItemId) {
-          el.setAttribute("material", {
-            color: "#0077CC",
-            opacity: 1,
-          });
-        }
-      }
-    });
+    
   },
 });
